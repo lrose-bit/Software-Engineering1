e6e6258b15fb50d60cc0dfe7de9179b2c1b701ca
lrose-bit <75938945+lrose-bit@users.noreply.github.com>
2021-08-04 20:58:56 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,3 +1,9 @@
+/*
+Autor:Lars Rose
+
+ */
+
+
 import com.fazecast.jSerialComm.SerialPort;
 import com.fazecast.jSerialComm.SerialPortDataListener;
 import com.fazecast.jSerialComm.SerialPortEvent;
@@ -9,10 +15,10 @@ public class Main {
 
     static SerialPort comPort;
     static String stringBuffer;
-    static String output = "";
-    static String coordinates;
-    static double widthDegree;
-    static double lengthDegree;
+    static String ausgabe = "";
+    static String koordinaten;
+    static double breitenGrad;
+    static double laengenGrad;
     static int i = 0;
 
     private static final class DataListener implements SerialPortDataListener {
@@ -30,14 +36,14 @@ public class Main {
             byte[] newData = new byte[comPort.bytesAvailable()];
             int numRead = comPort.readBytes(newData, newData.length);
             stringBuffer = new String(newData, 0, numRead);
-            //Daten aus stringBuffer in output schreiben
-            output += stringBuffer;
+            //Daten aus stringBuffer in ausgabe schreiben
+            ausgabe += stringBuffer;
             //System.out.println(stringBuffer);
 
         }
     }
 
-    static public void main(String[] args) throws InterruptedException {
+    static void koordinateAuslesen() throws InterruptedException {
         //Port mit GpsMaus aus Array von allen angeschlossenen Ports öffnen
         // 2 Manuell eingetragen da das Port an dritter Stelle der Com Ports ist(kommt auf die Stelle im eigenen Pc an)
         comPort = SerialPort.getCommPorts()[2];
@@ -55,17 +61,17 @@ public class Main {
         TimeUnit.SECONDS.sleep(1);
         comPort.closePort();
 
-        //output nach dem Passenden NMEA-Datensatz filtern
-        //System.out.println("output: " + output);
-        int index = output.indexOf("GNGGA");
+        //ausgabe nach dem Passenden NMEA-Datensatz filtern
+        //System.out.println("ausgabe: " + ausgabe);
+        int index = ausgabe.indexOf("GNGGA");
         //System.out.println(index);
 
-        //Auf Basis der Index-Position Substring von dem output erstellen
-        coordinates = output.substring(index, index + 76);
-        //System.out.println("FilterString: " + coordinates);
+        //Auf Basis der Index-Position Substring von dem ausgabe erstellen
+        koordinaten = ausgabe.substring(index, index + 76);
+        //System.out.println("FilterString: " + koordinaten);
 
         //Substring bei den Kommas spliten um so die einzelnen Teile verwenden zu können
-        String[] sentenceParts = coordinates.split(",");
+        String[] sentenceParts = koordinaten.split(",");
 
         //Überprüfung des Splits
         /*
@@ -75,13 +81,19 @@ public class Main {
         */
 
         //Breiten- und LaengenGrad von String nach Double parsen und /100 rechnen um so die Dezimalgrad darstellung
-        //von den coordinates zu haben
-        widthDegree = Double.parseDouble(sentenceParts[2]) / 100;
-        lengthDegree = Double.parseDouble(sentenceParts[4]) / 100;
+        //von den koordinaten zu haben
+        breitenGrad = Double.parseDouble(sentenceParts[2]) / 100;
+        laengenGrad = Double.parseDouble(sentenceParts[4]) / 100;
         //Breiten- und Laengengrad der aktuellen Position ausgeben
-        System.out.println("Breitengrad: " + widthDegree + "\nLängenGrad: " + lengthDegree);
+        System.out.println("Breitengrad: " + breitenGrad + "\nLängenGrad: " + laengenGrad);
         //Uhrzeit von der Gps-abfrage
         System.out.println(sentenceParts[1].substring(0, 2) + ":" + sentenceParts[1].substring(2, 4) + ":" + sentenceParts[1].substring(4, 6) + " UTC");
+
+    }
+
+    static public void main(String[] args) throws InterruptedException {
+
+        koordinateAuslesen();
 
     }
 }
