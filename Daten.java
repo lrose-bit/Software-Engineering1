9507dd1d3a72d019319974c3f0beb90d63fbf919
joket001 <84315589+joket001@users.noreply.github.com>
2021-08-06 00:14:28 +0200

    Add files via upload

--- Daten.java
+++ Daten.java
@@ -6,90 +6,106 @@ import java.io.FileWriter;
 import java.util.ArrayList;
 
 public class Daten {
-	
-	//ArrayList der Koordinaten
-	private ArrayList<String> position = new ArrayList<String>();
-	
-	//Konstruktor
-		Daten(ArrayList<String> position){
-			this.position = position;
-		}
-	
-	//Methode zum Speichern der Positionen in eine .csv Datei
-	public void  positionSpeichern() {
-		
-		try {	
 
-			File daten = new File("daten.csv"); //Datei aufrufen
-			daten.delete(); //Lösche Datei "Daten" falls vorhanden
-			daten.createNewFile(); //Datei erstellen falls nicht vorhanden
-			
-			//Übergebe alle Positionen aus der ArrayList in Daten.csv
+	// ArrayList der Koordinaten
+	private ArrayList<String> position = new ArrayList<String>();
+
+	// Konstruktor
+	Daten(ArrayList<String> position) {
+		this.position = position;
+	}
+
+	// Methode zum Speichern der Positionen in eine .csv Datei
+	public void positionSpeichern() {
+
+		try {
+
+			File daten = new File("daten.csv"); // Datei aufrufen
+			daten.delete(); // Lösche Datei "Daten" falls vorhanden
+			daten.createNewFile(); // Datei erstellen falls nicht vorhanden
+
+			// Übergebe alle Positionen aus der ArrayList in Daten.csv
 			PrintWriter pw = new PrintWriter(new FileWriter(daten));
-			for(String a : position) {
+			for (String a : position) {
 				pw.println(a);
 			}
-			
-			pw.flush(); //Schreibe und speicher String's in die Datei
-			pw.close(); //Schließe PrintWriter
-			
-		}catch(Exception e) {
-				e.printStackTrace(); //Fehlerbeschreibung
-		}
-		
-	}
-	
-	//Methode .csv lesen
-	public  ArrayList<Class_GpsKoordinate> positionLesen() {
 
+			pw.flush(); // Schreibe und speicher String's in die Datei
+			pw.close(); // Schließe PrintWriter
+
+		} catch (Exception e) {
+			e.printStackTrace(); // Fehlerbeschreibung
+		}
+
+	}
+
+	// Methode .csv lesen
+		public ArrayList<Class_GpsKoordinate> positionLesen() {
+
+//		Testen mit TestMain
+//		public ArrayList<TestMain> positionLesen() {
+	
 		// Einlesen des Files und spliten
 		FileReader daten = null;
 		BufferedReader buff = null;
 		String zeile;
 		ArrayList<String> zeilen = new ArrayList<String>();
-		
+
 		try {
-			daten = new FileReader("daten.csv"); //Greife auf Daten.csv zu
-			buff = new BufferedReader(daten); //Lese Daten.csv aus
-			
-			//Schleife die solange alle Werte in ein Array speichert bis keine mehr Vorhanden sind
+			daten = new FileReader("daten.csv"); // Greife auf Daten.csv zu
+			buff = new BufferedReader(daten); // Lese Daten.csv aus
+
+			// Schleife die solange alle Werte in ein Array speichert bis keine mehr
+			// Vorhanden sind
 			while ((zeile = buff.readLine()) != null) {
-				//System.out.println(zeile); // Kontrolle was eingelesen wurde
-				
+				System.out.println(zeile); // Kontrolle was eingelesen wurde
+
 				zeilen.add(zeile);
-				
+
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
-	}
-	
+		}
+				
 
-		//ArrayList für die Zurückgabe eines im passenden Datenformat (ArrayList mit Objekten vom Datentyp Double)
+		// ArrayList für die Zurückgabe im passenden Datenformat (ArrayList mit
+		// Objekten vom Datentyp Double)
 		ArrayList<Class_GpsKoordinate> gpsKoordinate = new ArrayList<Class_GpsKoordinate>();
-	
-		//Splite die ArrayList position in längengrad und breitengrad		
-		for(String grad : zeilen ) {
+		
+//		Testen mit TestMain
+//		ArrayList<TestMain> gpsKoor = new ArrayList<TestMain>();
+				
+		// Splite die ArrayList position in längengrad und breitengrad
+		for (String grad : zeilen) {
 			String[] gra = grad.split("!");
-			
-			//Umwandelung von String in Double
+
+			// Umwandelung von String in Double
 			double laengenGrad = Double.parseDouble(gra[0]);
 			double breitenGrad = Double.parseDouble(gra[1]);
 			System.out.println(laengenGrad);
 			System.out.println(breitenGrad);
-		
-			//Erstelle neues Objekt vom Typ 
+
+			// Erstelle neues Objekt vom Typ
 			Class_GpsKoordinate koordinateEins = new Class_GpsKoordinate(laengenGrad, breitenGrad);
 			gpsKoordinate.add(koordinateEins);
+	
+			// TESTEN Erstelle neues Objekt vom Typ
+			/*
+			TestMain koordinateEins = new TestMain(laengenGrad, breitenGrad);
+			gpsKoor.add(koordinateEins);
+			*/
 			
-			System.out.println(gpsKoordinate.get(1).getDouble_lat());
-			System.out.println(gpsKoordinate.get(1).getDouble_lon());
-			
-			}
-			
-			//Gebe gpsKoordinate zurück
-			return gpsKoordinate;		
-		
-	}
-		
-}
+			System.out.println(gpsKoordinate.get(1).getDouble_breitenGrad());
+			System.out.println(gpsKoordinate.get(1).getDouble_laengenGrad());
 
+
+		}
+
+		// Gebe gpsKoordinate zurück
+		return gpsKoordinate;
+
+//		TESTEN return
+//		return gpsKoor;
+	}
+
+}
--- TestMain.java
+++ TestMain.java
@@ -1,63 +1,31 @@
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class TestMain {
 	
+	public TestMain(double laengenGrad, double breitenGrad) {
+		
+	}
+
 	public static void main(String[] args){
 		
 			//ArrayList zum speichern der Koordinaten
 			ArrayList<String> position = new ArrayList<String>();
-		
-			//TESTEN - Koordinaten (Breite = b / Länge = l) um die Klasse "Daten" unabhängig zu testen.
-			String b1 = "51° 21' 59.0\" N";
-			String l1 = "7° 29' 51.3\" O";
-			String b2 = "51° 22' 6.0\" N";
-			String l2 = "7° 41' 14.5\" O";
-			String b3 = "51° 33' 42.8\" N";
-			String l3 = "8° 6' 46.8\" O";
-			String b4 = "51° 21' 42.5\" N";
-			String l4 = "8° 17' 45.3\" O";
+			String pos1 = "51.152565!11.809919";
+			String pos2 = "52.152565!12.809919";
 			
-			//Array zum zwischenspeichern der Testdaten Breite
-			String [] testB = {b1, b2, b3, b4};
-			
-			//Array zum zwischenspeichern der Testdaten Laenge
-			String [] testL = {l1, l2, l3, l4};
-			
-			//For-Schleife: Zeilenindex => i, Spaltenindex => j
-			for(int i = 0; i < testB.length; i++) {
-				int j = i;
-				
-				//Array String zu Single String breite
-				StringBuilder b = new StringBuilder();
-				b.append(testB[i]);
-				String breite = b.toString();
-//				System.out.println(breite);
-				
-				//Array String zu Single String laenge
-				StringBuilder l = new StringBuilder();
-				l.append(testL[j]);
-				String laenge = l.toString();
-//				System.out.println(laenge);
-				
-				//Methode - Methode Koordinate aus Koordinaten aufrufen
-				Koordinaten positionen = new Koordinaten(breite, laenge);
-				
-				//Rückgabewert von Methode Koordinate
-				String resultPosition = positionen.koordinate();
-//				System.out.println(resultPosition);
-				
-				//Schreibe Koordinate in ArrayList
-				position.add(resultPosition);
-				
-				}
-			
-//			System.out.println(position);
-			
+			//Position zur ArrayList hinzufügen
+			position.add(pos1);
+			position.add(pos2);
+
 			//Methode positionSpeichern aufrufen
-			Daten positionTest = new Daten(position);
-			positionTest.positionSpeichern();
+			Daten daten = new Daten(position);
+			daten.positionSpeichern();
+			
+			//Methode positionSpeichern ausführen
+			System.out.println(daten.positionLesen());
 				
-		}
+		
+	}
 
 }
-
